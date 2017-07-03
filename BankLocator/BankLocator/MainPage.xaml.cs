481aa82b4ac5f247bd6c2dc0d60ce3468c3c0a5e
Nicholas VDB <Nicholas VDB>
2017-07-03 19:52:24 +0200

    Add project files.

---
+++ BankLocator/BankLocator/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BankLocator
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
